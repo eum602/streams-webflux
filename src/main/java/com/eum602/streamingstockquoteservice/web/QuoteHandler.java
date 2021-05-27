36d744041a6eead6020a42cc30470ceaecf6baa8
Erick <eum602@gmail.com>
2021-05-27 11:12:04 -0500

    adds handler for quote objects - it returns either a json body of quotes or a stream of quotes

---
+++ src/main/java/com/eum602/streamingstockquoteservice/web/QuoteHandler.java
@@ -0,0 +1,43 @@
+package com.eum602.streamingstockquoteservice.web;
+
+import com.eum602.streamingstockquoteservice.model.Quote;
+import com.eum602.streamingstockquoteservice.service.QuoteGeneratorService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Mono;
+
+import java.time.Duration;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.http.MediaType.APPLICATION_NDJSON;
+import static org.springframework.web.reactive.function.server.ServerResponse.ok;
+
+@Slf4j
+@RequiredArgsConstructor
+@Component
+public class QuoteHandler {
+    private final QuoteGeneratorService quoteGeneratorService; //thanks to @RequiredArgsConstructor the will inject the QuoteGeneratorService
+    //at runtime because it is tagged with @Service
+
+    public Mono<ServerResponse> fetchQuotes(ServerRequest request) {
+        int size = Integer.parseInt(request.queryParam("size").orElse("10")); //if the query param is not present
+        //the default it to '10'
+        return ok()
+                .contentType(APPLICATION_JSON)
+                .body(quoteGeneratorService.fetchQuoteStream(Duration.ofMillis(100l))
+                        .take(size), Quote.class
+                );//will return a json body of quotes
+    }
+
+    public Mono<ServerResponse> streamQuotes(ServerRequest request) {
+        return ok()
+                .contentType(APPLICATION_NDJSON) //new line delimited json
+                .body(quoteGeneratorService.fetchQuoteStream(Duration.ofMillis(100l)), Quote.class
+                );//for a reactive stream -> it is going to return a continuous stream of quotes
+    }
+
+
+}
